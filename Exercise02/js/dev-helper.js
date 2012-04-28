5b9b72360bbd6b0cff3bc5ca9b80d4599609fc4a
chandermani <chandermani@hotmail.com>
2012-04-28 20:26:27 +0530

    made helper angular compatible

--- Exercise02/js/dev-helper.js
+++ Exercise02/js/dev-helper.js
@@ -20,50 +20,52 @@
   lorem += lorem;
   lorem += lorem;
 
-function addNewItems(num) {
-  for (var i = 0; i < num; i++) {
-    var item = {};
-    var item_id = Math.floor(Math.random() * 1000000000);
-    item.item_id = item_id;
-    item.key = item_id;
-    item.pub_name = feeds[Math.floor(Math.random()*feeds.length)];
-    item.pub_author = names[Math.floor(Math.random()*names.length)] + " " + names[Math.floor(Math.random()*names.length)];
-    item.title = "Item Title " + item_id.toString();
-    item.item_link = "http://url/" + item_id.toString();
-    item.feed_link = "http://url/" + item_id.toString();
-    item.content = "<p>" + item.title + "<p>" + lorem;
-    item.short_desc = item.content.substr(0, 128) + "...";
-    item.pub_date = new Date(1300000000000 + i * 86400000 + (86400000 * Math.random()));
-    if (Math.random() > 0.5) {
-      item.read = true;
-    }
-    if (Math.random() > 0.5) {
-      item.starred = true;
-    }
-    var emItem = WReader.Item.create(item);
-    WReader.dataController.addItem(emItem);
+  function addNewItems(num) {
+        //Scope that would be updated
+      var scope = angular.element('.summaries').scope();
+      for (var i = 0; i < num; i++) {
+          var item = new WReader.Item();
+          var item_id = Math.floor(Math.random() * 1000000000);
+          item.item_id = item_id;
+          item.key = item_id;
+          item.pub_name = feeds[Math.floor(Math.random() * feeds.length)];
+          item.pub_author = names[Math.floor(Math.random() * names.length)] + " " + names[Math.floor(Math.random() * names.length)];
+          item.title = "Item Title " + item_id.toString();
+          item.item_link = "http://url/" + item_id.toString();
+          item.feed_link = "http://url/" + item_id.toString();
+          item.content = "<p>" + item.title + "<p>" + lorem;
+          item.short_desc = item.content.substr(0, 128) + "...";
+          item.pub_date = new Date(1300000000000 + i * 86400000 + (86400000 * Math.random()));
+          if (Math.random() > 0.5) {
+              item.read = true;
+          }
+          if (Math.random() > 0.5) {
+              item.starred = true;
+          }
+          //since we are touching the view from outside angular context we need to call $apply
+          scope.$apply(scope.addItem(item));
+      }
   }
-}
 
-function addSingleNew() {
-  var item = {};
-  var i = Math.floor(Math.random() * 1000000000);
-  item.item_id = i;
-  item.key = i;
-  item.pub_name = feeds[Math.floor(Math.random()*feeds.length)];
-  item.pub_author = names[Math.floor(Math.random()*names.length)] + " " + names[Math.floor(Math.random()*names.length)];
-  item.title = "Item Title " + i.toString();
-  item.item_link = "http://url/" + i.toString();
-  item.feed_link = "http://url/" + i.toString();
-  item.content = "<p>" + item.title + "<p>" + lorem;
-  item.short_desc = item.content.substr(0, 128) + "...";
-  item.pub_date = new Date().toJSON();
-  item.read = false;
-  if (Math.random() > 0.5) {
-    item.starred = true;
+  function addSingleNew() {
+      var scope = angular.element('.summaries').scope();
+      var item = new WReader.Item();
+      var i = Math.floor(Math.random() * 1000000000);
+      item.item_id = i;
+      item.key = i;
+      item.pub_name = feeds[Math.floor(Math.random() * feeds.length)];
+      item.pub_author = names[Math.floor(Math.random() * names.length)] + " " + names[Math.floor(Math.random() * names.length)];
+      item.title = "Item Title " + i.toString();
+      item.item_link = "http://url/" + i.toString();
+      item.feed_link = "http://url/" + i.toString();
+      item.content = "<p>" + item.title + "<p>" + lorem;
+      item.short_desc = item.content.substr(0, 128) + "...";
+      item.pub_date = new Date().toJSON();
+      item.read = false;
+      if (Math.random() > 0.5) {
+          item.starred = true;
+      }
+      scope.$apply(scope.addItem(item));
   }
-  var emItem = WReader.Item.create(item);
-  WReader.dataController.addItem(emItem);
-}
 
 
