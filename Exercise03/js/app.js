b80ea22e69d2582a149fd2e16fcae6e75d2343c3
chandermani <chandermani@hotmail.com>
2012-04-29 12:27:12 +0530

    Init exercise3 for angular implementaion

--- Exercise03/js/app.js
+++ Exercise03/js/app.js
@@ -1,80 +1,59 @@
+// Create the all up Angular application
+var WReader = {}
 
+// Angular Object model for entry items
+WReader.Item = function () {
+    this.read = false;
+    this.starred = false;
+    this.item_id = null;
+    this.title = null;
+    this.pub_name = null;
+    this.pub_author = null;
+    this.pub_date = new Date(0);
+    this.short_desc = null;
+    this.content = null;
+    this.feed_link = null;
+    this.item_link = null
+};
 
+function DataController($scope) {
+    // content array for Anuglar's data
+    $scope.content = [];
 
-// Create the all up Ember application
-var WReader = Em.Application.create({
-  ready: function() {
-    // Call the superclass's `ready` method.
-    this._super();
-  }
-});
+    // Adds an item to the controller if it's not already in the controller
+    $scope.addItem = function (item) {
+        // Check to see if there are any items in the controller with the same
+        //  item_id already
+        var exists = this.content.filter(function (element) { return element.item_id == item.item_id; }).length;
+        if (exists === 0) {
+            // If no results are returned, we insert the new item into the data
+            // controller in order of publication date
+            var length = $scope.content.length, idx;
+            idx = this.binarySearch(Date.parse(item.pub_date), 0, length, $scope.content);
+            this.content.splice(idx, 0, item);
+        }
+    };
+    //Exercise 3.1
 
-// Ember Object model for entry items
-WReader.Item = Em.Object.extend({
-  read: false,
-  starred: false,
-  item_id: null,
-  title: null,
-  pub_name: null,
-  pub_author: null,
-  pub_date: new Date(0),
-  short_desc: null,
-  content: null,
-  feed_link: null,
-  item_link: null
-});
+    //Exercise 3.4 is not required.
 
-WReader.dataController = Em.ArrayController.create({
-  // content array for Ember's data
-  content: [],
+    //Exercise 3.5
 
-  // Adds an item to the controller if it's not already in the controller
-  addItem: function(item) {
-    // Check to see if there are any items in the controller with the same
-    //  item_id already
-    var exists = this.filterProperty('item_id', item.item_id).length;
-    if (exists === 0) {
-      // If no results are returned, we insert the new item into the data
-      // controller in order of publication date
-      var length = this.get('length'), idx;
-      idx = this.binarySearch(Date.parse(item.get('pub_date')), 0, length);
-      this.insertAt(idx, item);
-      return true;
-    } else {
-      // It's already in the data controller, so we won't re-add it.
-      return false;
-    }
-  },
+    // Binary search implementation that finds the index where a entry
+    // should be inserted when sorting by date.
+    $scope.binarySearch = function (value, low, high, data) {
+        var mid, midValue;
+        if (low === high) {
+            return low;
+        }
+        mid = low + Math.floor((high - low) / 2);
+        midValue = Date.parse(data[mid].pub_date);
 
-  // Binary search implementation that finds the index where a entry
-  // should be inserted when sorting by date.
-  binarySearch: function(value, low, high) {
-    var mid, midValue;
-    if (low === high) {
-      return low;
-    }
-    mid = low + Math.floor((high - low) / 2);
-    midValue = Date.parse(this.objectAt(mid).get('pub_date'));
-
-    if (value < midValue) {
-      return this.binarySearch(value, mid + 1, high);
-    } else if (value > midValue) {
-      return this.binarySearch(value, low, mid);
-    }
-    return mid;
-  }
-
-  /* Exercise 3.1 */
-
-});
-
-// View for the ItemsList
-WReader.SummaryListView = Em.View.extend({
-  tagName: 'article',
-  classNames: ['well', 'summary']
-
-  /* Exercise 3.4 */
-
-  /* Exercise 3.5 */
-
-});
+        if (value < midValue) {
+            return this.binarySearch(value, mid + 1, high, data);
+        } else if (value > midValue) {
+            return this.binarySearch(value, low, mid, data);
+        }
+        return mid;
+    };
+};
